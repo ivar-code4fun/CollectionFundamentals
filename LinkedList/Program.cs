961e9b2dc8df5290759a139abe246d58ad0edbec
Ravi Shankar <ravishankar.r1@gmail.com>
2017-02-02 21:24:00 -0800

    Added dictionary code.

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,6 +10,22 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+            var presidents = new LinkedList<string>();
+            presidents.AddLast("JFK");
+            presidents.AddLast("Lyndon B Johnson");
+            presidents.AddLast("Richard Nixon");
+            presidents.AddLast("Jimmy Carter");
+
+            LinkedListNode<string> nixon = presidents.Find("Richard Nixon");
+            presidents.AddAfter(nixon, "Gerald Ford");
+
+            presidents.RemoveFirst();
+            presidents.AddFirst("John F Kennedy");
+
+            foreach (string president in presidents)
+            {
+                Console.WriteLine(president);
+            }
         }
     }
 }
